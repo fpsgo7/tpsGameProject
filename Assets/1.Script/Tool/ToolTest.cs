fe8a8f087d123761597b8593de05dce1448fa289
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-02-02 18:50:34 +0900

    tooltest 성공

---
+++ Assets/1.Script/Tool/ToolTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTest : MonoBehaviour
+{
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GameObject explosion = EffectToolManager.Instance.EffectOneShot((int)EffectList.explosion,
+               this.transform.position );
+            //SoundToolManager.Instance.PlayOneShotEffect((int)stepSounds[index], transform.position, 0.2f);
+        }
+    }
+}
